5896b47f57db37351dd7dd0f541530d02100caaf
Ravi Kant <127819443+ravikant-216@users.noreply.github.com>
2023-11-07 10:08:25 +0530

    Add files via upload

---
+++ UserStory/src/utils/db.ts
@@ -0,0 +1,7 @@
+export const Items = [
+  { id: 1, name: 'Apples', price: 2, quantity: 15, total: 28 },
+  { id: 2, name: 'Peaches', price: 5, quantity: 11, total: 26 },
+  { id: 3, name: 'Bananas', price: 1, quantity: 19, total: 23 },
+  { id: 4, name: 'Orange', price: 10, quantity: 13, total: 20 },
+  { id: 5, name: 'Grave', price: 16, quantity: 12, total: 21 },
+]
